8204bf129d55dac265a7f33c9812cbad593d6fa1
Bogdan Nahornyy <bogdan.nahornyy@gmail.com>
2026-01-11 00:09:57 +0200

    fix(Docker): add trustProxies

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,6 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->trustProxies(at: '*');
         $middleware->alias([
             'jwt' => \App\Http\Middleware\JwtMiddleware::class,
             'role' => \App\Http\Middleware\RoleMiddleware::class,
